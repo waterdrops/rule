320b3d300343a01c8df59f75fcbdc75c608589d2
Warren <weiwei.liu@dena.com>
2021-03-23 15:32:49 +0800

    update the CEO_Weekly.js file

--- Surge/JS/CEO_weekly.js
+++ Surge/JS/CEO_weekly.js
@@ -1,4 +1,4 @@
-let obj = JSON.parse($response.body);
+let obj = $response.body;
 obj.data["is_vip"] = 1;
 obj.data["vip_end_time"] = "2066-06-06";
-$done({body: JSON.stringify(obj)});
+$done({body: obj});
